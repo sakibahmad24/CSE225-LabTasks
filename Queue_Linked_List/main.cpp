140b821797330d6b99cdae1f70e3020bf7995600
sakibahmad24 <kazi.sakib@northsouth.edu>
2019-02-27 19:52:11 +0600

    linked list using queue

---
+++ Queue_Linked_List/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "quetype.h"
+#include "quetype.cpp"
+
+using namespace std;
+
+int main()
+{
+    QueType <int> qt;
+
+    qt.Enqueue(10);
+    qt.Enqueue(1);
+    qt.Enqueue(156);
+    qt.Enqueue(2);
+
+    qt.printQue();
+
+}
