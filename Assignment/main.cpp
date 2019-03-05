5316785adc8995ee6e8a0bc17826c54a8a8a0b13
sakibahmad24 <kazi.sakib@northsouth.edu>
2019-03-05 06:09:19 +0600

    decimal to binary done

--- Assignment/main.cpp
+++ Assignment/main.cpp
@@ -29,7 +29,32 @@ int binToDec(int binaryNum)
     return (binaryNum % 10 + binToDec(binaryNum/10)*2);
 }
 
+int decToBin(int decNum)
+{
+    if (decNum==0)
+        return 0;
+    else
+       {
+         return
+            (decNum%2 + 10 * decToBin(decNum/2));
+       }
+}
 
+/*void decToHex(int decNum2){
+    int hex=0;
+    if(!decNum2)
+        return;
+    else {
+        hex=decNum2%16;
+        decToHex(decNum2/16);
+    }
+    if(hex>9)
+        cout<<"A"<< hex-10 <<endl;
+    else
+        cout<<hex<<endl;
+    }
+
+*/
 
 
 
@@ -38,6 +63,10 @@ int main()
 {
 
     //code for finding fibonacci number starts
+    /*
+
+
+
     int n;
 
     cout<<"The the value of n to get the fibonacci number"<<endl;
@@ -74,6 +103,26 @@ int main()
     cout<<binToDec(binaryNum)<<endl;
 
     //code for bin to dec ends
+
+    //code for dec to bin starts
+
+    int decNum;
+    cout<<"::Enter decimal number:: "<<endl;
+    cin>>decNum;
+    cout<<"::The binary equivalent is:: "<<decToBin(decNum);
+    cout<<""<<endl;
+    //code for dec to bin ends
+
+    */
+
+    int decNum2=0;
+
+    cout<<"::Enter a decimal number to convert into hex::"<<endl;
+    cin>>decNum2;
+    cout<<"::The converted number in hex is::"<<endl;
+    decToHex(decNum2);
+
+
 
     return 0;
 
